44cfcb155299d54b089f5923eeed2dbf96439f0b
nugrhrizki <nugraha07rizki@gmail.com>
2023-10-02 11:38:00 +0700

    spice things up

--- src/main.js
+++ src/main.js
@@ -33,39 +33,47 @@ onMount(() => {
   });
 });
 
-const Hasil = () => html`
-  <label for="bulan">
-    Jumlah yang perlu ditabung jika ingin mendapatkan Rp.
-    ${() => formatNumber(target().toString())} dalam
-    ${() => formatNumber(bulan().toString())} bulan:
-    <div class="flex w-full">
-      <div class="pre-input">Rp</div>
-      <input id="hasil" />
-    </div>
-  </label>
-`;
+function Hasil() {
+  return html`
+    <label for="bulan">
+      Jumlah yang perlu ditabung jika ingin mendapatkan Rp.
+      ${() => formatNumber(target().toString())} dalam
+      ${() => formatNumber(bulan().toString())} bulan:
+      <div class="flex w-full">
+        <div class="pre-input">Rp</div>
+        <input id="hasil" />
+      </div>
+    </label>
+  `;
+}
 
-const App = () => html`
-  <div class="container my-8">
-    <h1>Kalkulator Menabung Bulanan</h1>
-    <div class="grid">
-      <label for="target">
-        Target Akhir Tabungan:
-        <div class="flex w-full">
-          <div class="pre-input">Rp</div>
-          <input id="target" />
-        </div>
-      </label>
-      <label for="bulan">
-        Bulan:
-        <div class="flex w-full">
-          <input id="bulan" />
-          <div class="post-input">Bulan</div>
-        </div>
-      </label>
+function App() {
+  const [count, setCount] = createSignal(0);
+
+  setInterval(() => setCount(count() + 1), 1000);
+
+  return html`
+    <div class="container my-8">
+      <h1>Kalkulator Menabung Bulanan ${count}</h1>
+      <div class="grid">
+        <label for="target">
+          Target Akhir Tabungan:
+          <div class="flex w-full">
+            <div class="pre-input">Rp</div>
+            <input id="target" />
+          </div>
+        </label>
+        <label for="bulan">
+          Bulan:
+          <div class="flex w-full">
+            <input id="bulan" />
+            <div class="post-input">Bulan</div>
+          </div>
+        </label>
+      </div>
+      ${Hasil()}
     </div>
-    ${Hasil()}
-  </div>
-`;
+  `;
+}
 
 render(App(), "#app");
